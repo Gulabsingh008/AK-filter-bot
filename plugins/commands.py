ea39ea184dfb4bef0c42f3fc0313e9e55bc49079
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-08-02 18:50:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -94,7 +94,7 @@ async def start(client, message):
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [[
-            InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url=invite_link.invite_link)
+            InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url='https://t.me/movvvvvviess https://t.me/download_movies_wabseries')
         ]]
         if message.command[1] != "subscribe":
             if REQUEST_TO_JOIN_MODE == True:
