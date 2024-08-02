058c68ada616219f4d146d2f6c976c33542dbbd2
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-08-02 06:41:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://afmovies013:2ogoa47o7uge8SHt@cluster0.rpk5lsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://akmoviesworld:TEXTin1@@akmoviesworld.rctctox.mongodb.net/?retryWrites=true&w=majority&appName=akmoviesworld")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
