385c9d03685907c05c0bf3752c81013387831a80
AF <gulabsinghrajput008@gmail.com>
2025-02-26 13:07:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,12 +97,12 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'publicearn.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '11817fa2a85406a7cde102d27a8919e1e65fda60')
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'Modijiurl.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'fcfe0a1c21c33dcdc17d0de686c53c778a39b90c')
 # if verify second shortner is True then fill below url and api
-VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'publicearn.com')
-VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '11817fa2a85406a7cde102d27a8919e1e65fda60')
+VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'Modijiurl.com')
+VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'fcfe0a1c21c33dcdc17d0de686c53c778a39b90c')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/download_movies_wabseries')
 
 # Shortlink Info
